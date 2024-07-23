942e1c9a89ffe34534bcbf4eb9482985f7617c8d
Deeksha Rungta <deeksharu1902@gmail.com>
2024-07-23 19:12:50 +0400

    fix: minor fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,11 @@ import Head from "next/head";
 const quicksand = Quicksand({ subsets: ["latin"] });
 
 export default function Home() {
+  const encodedImageUrl =
+    "https://res.cloudinary.com/dczghbro7/image/upload/v1721746189/og-image_mzcne9.png".replace(
+      /\//g,
+      "%2F"
+    );
   return (
     <>
       <Head>
@@ -15,18 +20,12 @@ export default function Home() {
         <meta name="description" content="Your description here" />
         <meta property="og:title" content="Deq.fi" />
         <meta property="og:description" content="Your description here" />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dczghbro7/image/upload/v1721746189/og-image_mzcne9.png"
-        />
+        <meta property="og:image" content={encodedImageUrl} />
         <meta property="og:url" content="https://your-website-url.com" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Deq.fi" />
         <meta name="twitter:description" content="Your description here" />
-        <meta
-          name="twitter:image"
-          content="https://res.cloudinary.com/dczghbro7/image/upload/v1721746189/og-image_mzcne9.png"
-        />
+        <meta name="twitter:image" content={encodedImageUrl} />
       </Head>
       <main
         className={`flex min-h-screen flex-col items-center justify-between ${quicksand.className} overflow-hidden`}
